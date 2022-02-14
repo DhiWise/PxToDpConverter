afc1183b044a3c33fb60596b08b631512bcee8fc
divyabhayani <divya.bhayani@dhiwise.com>
2022-02-14 13:55:17 +0530

    Done : Dimension file changes

--- src/main/kotlin/util/Constant.kt
+++ src/main/kotlin/util/Constant.kt
@@ -16,8 +16,50 @@ const val KEY_VIEWPORT_HEIGHT = "viewport_height"
 
 const val DIGITS_AFTER_DECIMAL = 2
 
-val DIMENSION_RANGE_WIDTH = 1..820
-val DIMENSION_RANGE_HEIGHT = 1..1080
+val DIMENSION_RANGE_WIDTH = 1..884
+val DIMENSION_RANGE_HEIGHT = 1..1104
+
+val DIMENSION_WIDTH_LIST= listOf(
+    360,
+    375,
+    384,
+    385,
+    392,
+    393,
+    411,
+    412,
+    414,
+    768,
+    884,
+)
+
+val DIMENSION_HEIGHT_LIST= listOf(
+    640 ,
+    720 ,
+    731 ,
+    732 ,
+    740 ,
+    780,
+    786 ,
+    800 ,
+    808 ,
+    823 ,
+    830 ,
+    846 ,
+    851 ,
+    854 ,
+    869 ,
+    873 ,
+    883 ,
+    892 ,
+    906 ,
+    914 ,
+    915 ,
+    919 ,
+    1004 ,
+    1076 ,
+    1104 ,
+)
 
 val DIMENSION_WIDTH = 375.0
 
